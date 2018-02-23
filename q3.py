30439c1843e31ad614f85e9d5dd6e9e78ac3657b
patniharshit <patniharshit@gmail.com>
2018-02-23 17:02:24 +0530

    cosine similarity

--- q3.py
+++ q3.py
@@ -4,6 +4,7 @@ import os.path
 #from nltk.stem import PorterStemmer
 import math
 from scipy.linalg import svd
+from scipy.spatial.distance import cosine
 
 #ps = PorterStemmer()
 
@@ -33,7 +34,6 @@ for direc in listdir('./q2data/train'):
 
 dictkeys = word_dict.keys()
 #dictkeys.sort()
-print "dict formed"
 
 A = np.zeros([len(dictkeys), doc_count])
 
@@ -44,18 +44,46 @@ for i, k in enumerate(dictkeys):
 
 WordsPerDoc = np.sum(A, axis=0)
 DocsPerWord = np.sum(np.asarray(A > 0), axis=1)
-rows, cols = A.shape
 
-print "computing tf-idf"
-for i in range(rows):
-    for j in range(cols):
-            A[i,j] = (A[i,j] / WordsPerDoc[j]) * math.log(1 + (float(cols) / DocsPerWord[i]))
-
-print "tfidf done"
+for i in range(A.shape[0]):
+    for j in range(A.shape[1]):
+            A[i,j] = (A[i,j] / WordsPerDoc[j]) * math.log(1 + (float(A.shape[1]) / DocsPerWord[i]))
 
 A = A.transpose()
-
 #w, v = np.linalg.eig(np.matmul(A, A.transpose()))
-u, s, vh = np.linalg.svd(A, full_matrices=False)
-print u, s, vh
-#import ipdb; ipdb.set_trace()
+umat, smat, vh = np.linalg.svd(A, full_matrices=False)
+
+discard = 100
+
+smat = smat[:-discard]
+umat = umat[:,:-discard]
+vh = vh[:-discard,:]
+
+locs = './q2data/train/1/'
+files = listdir(locs)
+for filename in files[(80*len(files))/100+1:(80*len(files))/100+2]:
+    print filename
+    fp_data = open(locs+filename,'r')
+    tokens = ((fp_data.read())).split()
+    doc_vec = np.zeros(len(dictkeys))
+
+    word_count = 0
+    for w in tokens:
+        w = w.lower().translate(None, ignorechars)
+        #w = ps.stem(w)
+        if w in stopwords:
+            continue
+        elif w in dictkeys:
+            word_count += 1
+            doc_vec[dictkeys.index(w)] += 1
+
+    for idx in range(len(dictkeys)):
+        doc_vec[idx] = (doc_vec[idx] / word_count) * math.log(1 + (float(doc_count+1) / DocsPerWord[i]+doc_vec[idx]))
+
+    reduced_vec = np.matmul(doc_vec, vh.transpose())
+
+    similarity = []
+    for i, uvec in enumerate(umat):
+        similarity.append((1 - cosine(uvec, reduced_vec), i))
+
+    print sorted(similarity)[-10:]
